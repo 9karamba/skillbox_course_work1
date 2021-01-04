2a693f3dba2bb214d53117c860aa0d3929c4db33
9karamba <nas_bel@list.ru>
2021-01-04 16:55:33 +0300

    update product

--- include/add_product.php
+++ include/add_product.php
@@ -1,11 +1,18 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'].'/include/global_func.php';
 
+$uploaddir = '/uploads/';
+
+if(!is_dir($_SERVER['DOCUMENT_ROOT'] . $uploaddir)){
+    mkdir($_SERVER['DOCUMENT_ROOT'] . $uploaddir, 0777);
+}
+
 if(isset( $_POST["add-product"] )){
-    if ( empty( $_POST["product-name"] ) || empty( $_POST["product-price"] ) || !isset( $_FILES["product-photo"] ) || empty( $_FILES["product-photo"]["name"] ) ) {
-        $error = "Данные о товаре и фотография обязательны для заполнения.";
+    if ( empty( $_POST["product-name"] ) || empty( $_POST["product-price"] ) ) {
+        $error = "Данные о товаре обязательны для заполнения.";
     }
     else{
+        $id = isset($_POST["product-id"]) ? intval( htmlspecialchars($_POST["product-id"]) ) : false;
         $name = htmlspecialchars($_POST["product-name"]);
         $price = intval( htmlspecialchars($_POST["product-price"]) );
         $new = intval( isset($_POST["new"]) );
@@ -18,17 +25,50 @@ if(isset( $_POST["add-product"] )){
         elseif($price <= 0){
             $error = "Цена должна быть числом и должна быть больше 0.";
         }
-        else{
-            $uploaddir = '/uploads/';
-
-            if(!is_dir($_SERVER['DOCUMENT_ROOT'] . $uploaddir)){
-                mkdir($_SERVER['DOCUMENT_ROOT'] . $uploaddir, 0777);
+        elseif (empty( $_FILES["product-photo"]["name"] ) && !is_numeric($id)){
+            $error = "Фотография не должна быть пустой.";
+        }
+        elseif (is_numeric($id)){
+            if(empty( $_FILES["product-photo"]["name"] )){
+                $path = $uploaddir . basename(htmlspecialchars($_POST["product-image"]));
             }
+            else{
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                $path = $uploaddir . basename($file['name']);
 
+                if(!move_uploaded_file( $file['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . $path)){
+                    $error = 'Ошибка загрузки фото.';
+                }
+                finfo_close($finfo);
+            }
+            $link = connectionDB();
+            $query = "UPDATE products SET name='{$name}', price='{$price}', new='{$new}', sale='{$sale}', photo='{$path}' WHERE id='{$id}'";
+            $result = getResultDB($link, $query);
+            if($result){
+                $categories = [];
+
+                foreach ($_POST["category"] as $category_id){
+                    $categories[]= "('{$id}','{$category_id}')";
+                }
+
+                $query ="DELETE FROM product_categories WHERE product_id = '$id'";
+                $result = getResultDB($link, $query);
+
+                if(!empty($categories)){
+                    $request = join(',', $categories);
+                    $query ="INSERT INTO product_categories (product_id, category_id) VALUES {$request};";
+                    $result = getResultDB($link, $query);
+                }
+            }
+            else{
+                $error = 'Не удалось обновить товар.';
+            }
+        }
+        else{
             $finfo = finfo_open(FILEINFO_MIME_TYPE);
-            $path = $_SERVER['DOCUMENT_ROOT'] . $uploaddir . basename($file['name']);
+            $path = $uploaddir . basename($file['name']);
 
-            if(!move_uploaded_file( $file['tmp_name'], $path)){
+            if(!move_uploaded_file( $file['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . $path)){
                 $error = 'Ошибка загрузки фото.';
             }
             finfo_close($finfo);
@@ -46,8 +86,8 @@ if(isset( $_POST["add-product"] )){
                         $categories[]= "('{$product_id}','{$category_id}')";
                     }
 
-                    $request = join(',', $categories);
                     if(!empty($categories)){
+                        $request = join(',', $categories);
                         $query ="INSERT INTO product_categories (product_id, category_id) VALUES {$request};";
                         $result = getResultDB($link, $query);
                     }
@@ -56,6 +96,22 @@ if(isset( $_POST["add-product"] )){
                     $error = 'Не удалось добавить товар.';
                 }
             }
+        }
+    }
+}
+elseif(isset( $_GET["product-id"] )){
+    $id = intval( htmlspecialchars($_GET["product-id"]) );
+
+    $link = connectionDB();
+    $query ="SELECT * FROM products
+        LEFT JOIN product_categories ON products.id = product_categories.product_id
+        WHERE products.id = '$id'";
+    $result = getResultDB($link, $query);
+
+    $product = mysqli_fetch_all( $result, MYSQLI_ASSOC );
+    if($product){
+        foreach ($product as $item){
+            $product_categories[] = $item["category_id"];
         }
     }
 }
--- templates/admin/add.php
+++ templates/admin/add.php
@@ -14,18 +14,22 @@ include dirname(__FILE__) . '/../header.php';
             <?= $error ?>
         </p>
     <?php endif; ?>
-  <form class="custom-form" action="/admin/add" enctype="multipart/form-data" method="post" <?= !isset($error) ? 'hidden' : '' ?>>
-    <fieldset class="page-add__group custom-form__group">
+  <form class="custom-form" action="/admin/add/<?= isset($_GET["product-id"]) ? '?product-id=' . $_GET["product-id"] : '' ?>" enctype="multipart/form-data" method="post" <?= !isset($error) && isset($_POST["add-product"]) ? 'hidden' : '' ?>>
+      <?php if(isset($product) && $product ): ?>
+        <input type="hidden" name="product-id" value="<?= $product[0]['id'] ?>">
+        <input type="hidden" name="product-image" value="<?= $product[0]['photo'] ?>">
+      <?php endif; ?>
+      <fieldset class="page-add__group custom-form__group">
       <legend class="page-add__small-title custom-form__title">Данные о товаре</legend>
       <label for="product-name" class="custom-form__input-wrapper page-add__first-wrapper">
-        <input type="text" class="custom-form__input" name="product-name" id="product-name">
-        <p class="custom-form__input-label">
+        <input type="text" class="custom-form__input" name="product-name" id="product-name" value="<?= isset($product) && $product ? $product[0]["name"] : '' ?>">
+        <p class="custom-form__input-label" <?= isset($product) && $product ? 'hidden' : '' ?>>
           Название товара
         </p>
       </label>
       <label for="product-price" class="custom-form__input-wrapper">
-        <input type="text" class="custom-form__input" name="product-price" id="product-price">
-        <p class="custom-form__input-label">
+        <input type="text" class="custom-form__input" name="product-price" id="product-price" value="<?= isset($product) && $product ? $product[0]["price"] : '' ?>">
+        <p class="custom-form__input-label" <?= isset($product) && $product ? 'hidden' : '' ?>>
           Цена товара
         </p>
       </label>
@@ -34,9 +38,14 @@ include dirname(__FILE__) . '/../header.php';
       <legend class="page-add__small-title custom-form__title">Фотография товара</legend>
       <ul class="add-list">
         <li class="add-list__item add-list__item--add">
-          <input type="file" name="product-photo" id="product-photo" hidden="">
+          <input type="file" name="product-photo" id="product-photo" hidden="" value="<?= isset($product) && $product ? $product[0]["photo"] : '' ?>">
           <label for="product-photo">Добавить фотографию</label>
         </li>
+          <?php if( isset($product) && $product ): ?>
+              <li class="add-list__item add-list__item--active">
+                  <img src="<?= $product[0]["photo"] ?>">
+              </li>
+          <?php endif; ?>
       </ul>
     </fieldset>
     <fieldset class="page-add__group custom-form__group">
@@ -45,18 +54,18 @@ include dirname(__FILE__) . '/../header.php';
         <select name="category[]" class="custom-form__select" multiple="multiple">
           <option hidden="">Название раздела</option>
             <?php foreach (getCategories() as $category): ?>
-                <option value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
+                <option value="<?= $category['id'] ?>" <?= isset($product_categories) && in_array($category['id'], $product_categories) ? 'selected' : '' ?>><?= $category['name'] ?></option>
             <?php endforeach; ?>
         </select>
       </div>
-      <input type="checkbox" name="new" id="new" class="custom-form__checkbox">
+      <input type="checkbox" name="new" id="new" class="custom-form__checkbox" <?= isset($product) && $product && $product[0]["new"] ? 'checked' : '' ?>>
       <label for="new" class="custom-form__checkbox-label">Новинка</label>
-      <input type="checkbox" name="sale" id="sale" class="custom-form__checkbox">
+      <input type="checkbox" name="sale" id="sale" class="custom-form__checkbox" <?= isset($product) && $product && $product[0]["sale"] ? 'checked' : '' ?>>
       <label for="sale" class="custom-form__checkbox-label">Распродажа</label>
     </fieldset>
     <button class="button" name="add-product" type="submit">Добавить товар</button>
   </form>
-  <section class="shop-page__popup-end page-add__popup-end" <?= isset($error) ? 'hidden' : '' ?>>
+  <section class="shop-page__popup-end page-add__popup-end" <?= isset($error) || !isset($_POST["add-product"]) ? 'hidden' : '' ?>>
     <div class="shop-page__wrapper shop-page__wrapper--popup-end">
       <h2 class="h h--1 h--icon shop-page__end-title">Товар успешно добавлен</h2>
     </div>
